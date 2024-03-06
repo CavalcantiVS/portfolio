c1069998171b0f441770d4a8b8d5c86389d9de8a
João Victor de Souza Cavalcanti <jvdecavalcanti@gmail.com>
2024-03-06 14:53:48 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var myCarousel = new bootstrap.Carousel(document.getElementById('carouselExampleCaptions'), {
+  interval: 2000,  // Define o intervalo para 2 segundos
+  wrap: true       // Permite que o carrossel volte ao primeiro slide após o último
+});
